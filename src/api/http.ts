2ca5431e7d30c320918700bb95665f313d2c7b2b
qiuwenhui <1059713136@qq.com>
2025-10-19 10:16:10 +0800

    ✨ feat(http): http模块格式化

--- src/api/http.ts
+++ src/api/http.ts
@@ -1,26 +1,25 @@
-import type { AxiosInstance, AxiosResponse } from 'axios'
-import axios from 'axios'
+import type { AxiosInstance, AxiosResponse } from "axios";
+import axios from "axios";
 
 export const http: AxiosInstance = axios.create({
-  baseURL: '/api', // Replace with your API base URL
+  baseURL: "/api", // Replace with your API base URL
   timeout: 10000,
-  headers: { 'Content-Type': 'application/json' },
-})
+  headers: { "Content-Type": "application/json" },
+});
 
 http.interceptors.response.use(
   (response: AxiosResponse) => {
-    const { code, message, data } = response.data
+    const { code, message, data } = response.data;
 
     if (code === 0) {
-      return data
-    }
-    else {
-      console.error(message)
-      return Promise.reject(new Error(message))
+      return data;
+    } else {
+      console.error(message);
+      return Promise.reject(new Error(message));
     }
   },
   (error) => {
-    console.error('Request error:', error)
-    return Promise.reject(error)
-  },
-)
+    console.error("Request error:", error);
+    return Promise.reject(error);
+  }
+);
